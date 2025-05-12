06ee57800566c017111a047bfc5dd298462ecf8d
TriWahyuUtomo <tri@aisl.cs.tut.ac.jp>
2025-05-12 02:21:21 +0000

    finish robot state recorder

--- src/dataset_recorder/dataset_recorder/robot.py
+++ src/dataset_recorder/dataset_recorder/robot.py
@@ -1,79 +1,156 @@
-import json
-import numpy as np
-
-from typing import List
-from pathlib import Path
-
 import rospy
 import message_filters
+import tf2_ros
+import tf.transformations as tfs
+import numpy as np
 from sensor_msgs.msg import JointState
 
+from dataset_recorder.utils import map_subinfo_to_idx
+from dataset_recorder.msg import RobotBoolState
 
-class ArmRecorder:
+
+class RobotStateRecorder:
     """
-    Handles recording RGB images and PointCloud2 data from a Kinect-like
-    sensor running in ROS to a specified directory.
+    Handles recording of robot states: joint state, end-effector pose, gripper states.
     """
-    def __init__(self, output_dir, which_arm):
+    def __init__(self):
         """
         Initializes the recorder.
 
         Args:
             output_dir (str): The base directory where data will be saved.
-            which_arm (str): Which arm to record, either 'left' or 'right'.
         """
         rospy.loginfo(f"Initializing Kinect Recorder...")
 
-        self.which_arm = which_arm
-        self.output_dir = Path(output_dir).joinpath(which_arm).resolve()
+        self.joint_state_topic: str = rospy.get_param("~robot_state_topic", "/joint_states").lstrip('/')
+        self.gripper_state_topic: str = rospy.get_param("~gripper_state_topic", "/robot_gripper_state").lstrip('/')
 
-        robot_state_topic = rospy.get_param("~robot_state_topic", "/joint_states").lstrip('/')
-        self.robot_state_topic_name: str = robot_state_topic
+        self.left_eef_tf = rospy.get_param("~left_eef_tf_name", "LARM_LINK_EEF")
+        self.right_eef_tf = rospy.get_param("~right_eef_tf_name", "RARM_LINK_EEF")
+        self.world_tf = rospy.get_param("~world_tf_name", "WAIST")
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
-        self.robot_state_dir = self.output_dir / "robot_state"
-        self.robot_state_dir.mkdir(parents=True, exist_ok=True)
+        self.joint_names = []
 
-        self.robot_state_sub = None
+        self.joint_state_sub = None
+        self.gripper_state_sub = None
+        self.msg_idx_map = {}
 
     def setup(self, subscribers: list, subscriber_info: list):
         """Sets up the ROS subscribers."""
-        self.robot_state_sub = message_filters.Subscriber(self.robot_state_topic_name, JointState)
-        self.state_info_name = f"{self.which_arm}_arm/state"
-        subscribers.append(self.robot_state_sub)
-        subscriber_info.append({"name": self.state_info_name, "type": JointState})
+        self.joint_state_sub = message_filters.Subscriber(self.joint_state_topic, JointState)
+        self.joint_info_name = f"robot_state/joint"
+        subscribers.append(self.joint_state_sub)
+        subscriber_info.append({"name": self.joint_info_name, "type": JointState})
 
-    def save_robot_state(self, msgs: list, frame_id: str) -> str:
-        """Save image message to disk as PNG"""
-        filename = f"{frame_id}.json"
-        filepath = self.robot_state_dir / filename
+        self.gripper_state_sub = message_filters.Subscriber(self.joint_state_topic, RobotBoolState)
+        self.gripper_info_name = f"robot_state/gripper"
+        subscribers.append(self.gripper_state_sub)
+        subscriber_info.append({"name": self.gripper_info_name, "type": RobotBoolState})
+
+        msg: JointState = rospy.wait_for_message(self.joint_state_topic, JointState, timeout=1)
+        self.joint_names = list(msg.name)
+
+    def set_msg_idx_map(self, idx_map: dict):
+        self.msg_idx_map = idx_map
+
+    def get_attributes(self):
+        return {
+            "joint_names": self.joint_names,
+            "left_eef_tf": self.left_eef_tf,
+            "right_eef_tf": self.right_eef_tf,
+            "world_tf": self.world_tf
+        }
+
+    def get_pose_tf(self, tf_name: str, base_frame: str) -> tf2_ros.TransformStamped:
+        try:
+            tf_stamped: tf2_ros.TransformStamped = self.tf_buffer.lookup_transform(
+                base_frame, tf_name, rospy.Time(0), rospy.Duration(0.1)
+            )
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+            rospy.logwarn(f"[RobotStateRecorder] Could not get transform from '{base_frame}' to '{tf_name}': {e}")
+            return None
+        return tf_stamped
+
+    def get_pose_tf_mat(self, tf_name: str, base_frame: str):
+        tfstamped = self.get_pose_tf(tf_name, base_frame)
+        if tfs is None:
+            return None
+        
+        translation = [
+            tfstamped.transform.translation.x,
+            tfstamped.transform.translation.y,
+            tfstamped.transform.translation.z
+        ]
+    
+        rotation_quat = [
+            tfstamped.transform.rotation.x,
+            tfstamped.transform.rotation.y,
+            tfstamped.transform.rotation.z,
+            tfstamped.transform.rotation.w
+        ]
+
+        trans_mat = tfs.translation_matrix(translation)
+        rot_mat = tfs.quaternion_matrix(rotation_quat)
+        
+        # Combine the translation and rotation matrices
+        # M = T * R
+        tf_matrix = np.dot(trans_mat, rot_mat)
+        return list(tf_matrix.flatten())
+
+    def get_robot_state(self, msgs: list, frame_id: str) -> str:
+        """Returns robot state as a dictionary."""
 
         try:
-            msg: JointState = msgs[self.state_info_name]
-            state_dict = {
-                "header_stamp_sec": msg.header.stamp.to_sec(),
-                "name": list(msg.name),
-                "position": list(msg.position) if msg.position is not None else [],
-                "velocity": list(msg.velocity) if msg.velocity is not None else [],
-                "effort": list(msg.effort) if msg.effort is not None else [],
+            joint_msg: JointState = msgs[self.msg_idx_map[self.joint_info_name]]
+            gripper_msg: RobotBoolState = msgs[self.msg_idx_map[self.gripper_info_name]]
+
+            left_eef = self.get_pose_tf_mat(self.left_eef_tf, self.world_tf)
+            right_eef = self.get_pose_tf_mat(self.right_eef_tf, self.world_tf)
+
+            joint_states = {
+                "pos": list(joint_msg.position) if joint_msg.position is not None else [],
+                "vel": list(joint_msg.velocity) if joint_msg.velocity is not None else [],
+                "effort": list(joint_msg.effort) if joint_msg.effort is not None else [],
             }
-            with open(filepath, 'w') as f:
-                json.dump(state_dict, f, indent=4)
-            return filepath
+
+            state_dict = {
+                "joint": joint_states,
+                "left_eef": left_eef,
+                "right_eef": right_eef,
+                "left_gripper": bool(gripper_msg.left_gripper),
+                "right_gripper": bool(gripper_msg.right_gripper)
+            }
+            return state_dict
         except Exception as e:
-            rospy.logerr(f"Failed to save robot state frame {frame_id}: {e}")
+            rospy.logerr(f"Failed to retrieve robot state {frame_id}: {e}")
             raise
 
-class DualArmRecorder:
-    def __init__(self, output_dir):
-        self.output_dir = Path(output_dir).resolve()
-        self.left_recorder = ArmRecorder(output_dir, "left")
-        self.right_recorder = ArmRecorder(output_dir, "right")
 
-    def setup(self, subscribers: list, subscriber_info: list):
-        self.left_recorder.setup(subscribers, subscriber_info)
-        self.right_recorder.setup(subscribers, subscriber_info)
+if __name__ == "__main__":
+    rospy.init_node("robot_recorder", anonymous=True)
+    recorder = RobotStateRecorder()
 
-    def save_robot_state(self, msgs: list, frame_id: str) -> List[str]:
-        left_filepath = self.left_recorder.save_robot_state(msgs, frame_id)
-        right_filepath = self.right_recorder.save_robot_state(msgs, frame_id)
-        return [left_filepath, right_filepath]
+    subs, sub_info = [], []
+    recorder.setup(subs, sub_info)
+    recorder.set_msg_idx_map(map_subinfo_to_idx(sub_info))
+
+    attr = recorder.get_attributes()
+
+    frame_num = 0
+
+    def sync_callback(*msgs):
+        global frame_num
+        try:
+            frame_id = f"{frame_num:06d}"
+            robot_states = recorder.get_robot_state(msgs, frame_id)
+            frame_num += 1
+        except Exception as e:
+            rospy.logerr(f"Error saving data: {e}")
+
+    ts = message_filters.ApproximateTimeSynchronizer(
+        subs, queue_size=10, slop=0.1
+    )
+    ts.registerCallback(sync_callback)
+    rospy.spin()
