f0b0c97aa952cb2fc813c05b877390f281fba26d
TriWahyuUtomo <tri@aisl.cs.tut.ac.jp>
2025-05-26 05:55:37 +0000

    update after testing camera node

--- src/dataset_recorder/nodes/camera_recorder_node.py
+++ src/dataset_recorder/nodes/camera_recorder_node.py
@@ -6,6 +6,7 @@ import pickle
 
 from pathlib import Path
 from std_srvs.srv import Trigger, TriggerResponse, TriggerRequest
+from message_filters import ApproximateTimeSynchronizer
 from dataset_recorder.camera import DualKinectRecorder
 from dataset_recorder.utils import map_subinfo_to_idx
 
@@ -14,19 +15,29 @@ class CameraRecorder:
     def __init__(self, output_dir: Path):
         self.output_dir = output_dir
 
+        self.sync_rate: float = rospy.get_param("~sync_rate", 10.0)
+        self.sync_period = rospy.Duration(1.0 / self.sync_rate)
+        self.queue_size = rospy.get_param("~queue_size", 10)
+        self.slop = rospy.get_param("~slop", 0.1)
+        self.last_sync_time = rospy.Time(0)
+
         self.frame_idx = 0
         self.clip_name = None
         self.clip_dir = None
+        self.is_recording = False
 
         self.recorder = DualKinectRecorder(output_dir)
-        sub, sub_info = [], []
-        self.recorder.setup(sub, sub_info)
-        msg_idx_map = map_subinfo_to_idx(sub_info)
-        self.recorder.set_msg_idx_map(msg_idx_map)
+        self.sub, self.sub_info = [], []
+        self.recorder.setup(self.sub, self.sub_info)
+        self.msg_idx_map = map_subinfo_to_idx(self.sub_info)
+        self.recorder.set_msg_idx_map(self.msg_idx_map)
+
+        self.ts = ApproximateTimeSynchronizer(self.sub, queue_size=10, slop=0.1)
+        self.ts.registerCallback(self.sync_callback)
 
         self.record_service = rospy.Service("~start", Trigger, self.start_record)
         self.stop_service = rospy.Service("~stop", Trigger, self.stop_record)
-        self.is_recording = False
+        rospy.loginfo("Camera recorder is ready!")
 
     def start_record(self, req: TriggerRequest) -> TriggerResponse:
         if self.is_recording:
@@ -34,6 +45,7 @@ class CameraRecorder:
 
         self.is_recording = True
         timestamp = time.strftime("%Y%m%d_%H%M%S")
+        self.frame_idx = 0
 
         self.clip_name = timestamp
         self.clip_dir = self.output_dir / self.clip_name
@@ -51,31 +63,45 @@ class CameraRecorder:
             return TriggerResponse(success=False, message="Not currently recording")
 
         self.is_recording = False
+        self.frame_idx = 0
+        self.clip_name = None
+        self.clip_dir = None
 
         return TriggerResponse(
             success=True,
             message=f"Stopped recording. Recorded {self.frame_idx} frames.",
         )
 
-    def rate_callback(self, *msgs):
+    def sync_callback(self, *msgs):
         if not self.is_recording:
             return
 
+        current_time = rospy.Time.now()
+        interval = current_time - self.last_sync_time
+        # if interval < self.sync_period:
+        #     return
+
+        self.last_sync_time = current_time
+
         start_ts = time.perf_counter()
         output_file = self.msgs_dir / f"{self.frame_idx:06d}.pkl"
+        data = {
+            "timestamp": current_time.to_sec(),
+            "msgs": msgs,
+            "msg_idx_map": self.msg_idx_map,
+        }
         with output_file.open("wb") as f:
-            pickle.dump(msgs, f)
-        rospy.loginfo(f"{self.frame_idx}  {(time.perf_counter() - start_ts)*1000} ms")
+            pickle.dump(data, f)
+        rospy.loginfo(f"{self.frame_idx}  {interval.to_sec()*1000} ms")
         self.frame_idx += 1
 
 
 if __name__ == "__main__":
     try:
-        rospy.init_node("camera_recorder", anonymous=True)
+        rospy.init_node("camera_recorder")
         output_dir = Path(rospy.get_param("~save_dir", "/tmp/dataset_recordings"))
 
         recorder = CameraRecorder(output_dir)
-        recorder.start_record()
 
         rospy.spin()
     except rospy.ROSInterruptException:
